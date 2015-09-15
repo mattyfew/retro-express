cf3aa2f7572c55549d38ec0f60dde08493f583f9
Matt Fewer <mattfewer@hotmail.com>
2015-09-15 10:53:01 -0400

    server up and running

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var ejs = require('ejs');
 var urlencodedBodyParser = bodyParser.urlencoded({extended:false});
 //need to inlude DB
 var methodOverride = require('method-override');
 
 app.use(urlencodedBodyParser);
 app.use(methodOverride('_method'));
+app.set('view_engine', 'ejs');
 app.use(express.static('public'));
-
-app.get('/retro-express', function(req,res){
-	res.render('index.html')
-})
 
 app.get('/', function(req,res){
 	res.redirect('/retro-express')
 })
 
+app.get('/retro-express', function(req,res){
+	res.render('index.html.ejs')
+})
+
 app.listen(3000, function(){
-	console.log("listening on port 3000, bitch!")
+	console.log("Listening on port 3000")
 })
