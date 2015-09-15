91e394cbb4d51b0bef1a772eea3ce0d06dac7336
Spencer Denauski <oxideous@hotmail.com>
2015-09-15 15:09:48 -0400

    got the spaceship

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,6 @@
 console.log("APP.JS")
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 45, window.innerWidth/window.innerHeight, 0.1, 0 );
 ////DOM SETUP
 var renderer = new THREE.WebGLRenderer();
 var display = document.getElementById('game-display');
@@ -19,24 +19,57 @@ function onWindowResize() {
 	renderer.setSize( displayWidth, displayHeight );
 }
 
+////LIGHTS
+scene.add(new THREE.AmbientLight(0xCCCCCC));
+
+////SHAPES
+var rexShape = new THREE.Shape();
+rexShape.moveTo(356,258.1);
+rexShape.lineTo(315.3,220);
+rexShape.lineTo(306,201.3);
+rexShape.lineTo(296.7,220);
+rexShape.lineTo(256,258.1);
+rexShape.lineTo(277.8,258.1);
+rexShape.lineTo(299.4,258.1);
+rexShape.lineTo(306,271.3);
+rexShape.lineTo(312.6,258.1);
+rexShape.lineTo(334.2,258.1);
+
+//EXTRUTION FUNCTIONS
+var rexExtrusion = {
+	amount: 5,
+	bevelEnabled: false
+};
+
+
 
 ////GEOMETRY
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe:true } );
+//var cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
 
-camera.position.z = 5;
+
+var rexGeometry = new THREE.ExtrudeGeometry(rexShape, rexExtrusion);
+var rexMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe:true } );
+var rexMesh = new THREE.Mesh( rexGeometry, rexMaterial );
+rexMesh.position.set = THREE.Geometry.center;
+scene.add( rexMesh );
+
+
+camera.position.z = 1000;
 
 var render = function () {
 	requestAnimationFrame( render );
 
-	cube.rotation.x += 0.1;
-	cube.rotation.y += 0.1;
+//	cube.rotation.x += 0.1;
+//	cube.rotation.y += 0.1;
+	
+	rexMesh.rotation.x += 0.01;
+	rexMesh.rotation.y += 0.01;
 
 	renderer.render(scene, camera);
 };
 
 render();
-
 
