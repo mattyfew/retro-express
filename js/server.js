17bf34d5aa1d178e09f0af1e3575076f0cc189bb
Matt Fewer <mattfewer@hotmail.com>
2015-09-15 10:28:38 -0400

    added server.js

---
+++ js/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var body-parser = require('body-parser')
+var urlencodedBodyParser = bodyParser.urlencoded({extended:false});
+//need to inlude DB
+var methodOverride = require('method-override');
+
+app.use(urlencodedBodyParser);
+app.use(methodOverride('_method'));
+
+app.get('/retro-express', function(req,res){
+	res.render('index.html')
+})
+
+app.get('/', function(req,res){
+	res.redirect('/retro-express')
+})
+
+app.listen(3000, function(){
+	console.log("listening on port 3000, bitch!")
+})
